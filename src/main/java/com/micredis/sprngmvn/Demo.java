8111cd8a89e72f0ce841322cf9e4133b7de87522
micredis <micredis@gmail.com>
2017-10-05 15:32:38 +0300

    Changed Demo class: added new field Demo d. According to changes in Demo class, DemoRest was also changed (method date() now creates new Demo object with Demo field (default constructor)).

--- src/main/java/com/micredis/sprngmvn/Demo.java
+++ src/main/java/com/micredis/sprngmvn/Demo.java
@@ -6,14 +6,16 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Demo {
     private Integer a;
     private String b;
+    private Demo d;
 
     public Demo() {
 
     }
 
-    public Demo(Integer a, String b) {
+    public Demo(Integer a, String b, Demo d) {
         this.a = a;
         this.b = b;
+        this.d = d;
     }
 
     public Integer getA() {
@@ -24,11 +26,19 @@ public class Demo {
         return b;
     }
 
+    public Demo getD() {
+        return d;
+    }
+
     public void setA(Integer a) {
         this.a = a;
     }
 
     public void setB(String b) {
         this.b = b;
+    }
+
+    public void setD(Demo d) {
+        this.d = d;
     }
 }
